e877971be484b7c69aa0ea35735055df0ed3fa43
BALAJI POTHULA <balan.pothula@gmail.com>
2019-09-04 19:15:41 +0530

    Update install-sonarqube-server.sh

--- install-sonarqube-server.sh
+++ install-sonarqube-server.sh
@@ -15,13 +15,13 @@ sed -i 's/#sonar.embeddedDatabase.port=/sonar.embeddedDatabase.port=/'     $HOME
 comment
 
 # sonarqube downloading, extracting and running.
-curl -J -L https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-7.2.1.zip -o $HOME/sonarqube.zip    && \
-unzip -q $HOME/sonarqube.zip -d $HOME                                                                            && \
-rm -rf   $HOME/sonarqube.zip                                                                                     && \
-mv       $HOME/sonarqube-* $HOME/sonarqube                                                                       && \
-rm -rf $HOME/sonarqube/COPYING                                                                                   && \
-find     $HOME/sonarqube -name "*.txt"   -type f -delete                                                         && \
-rm -rf   $HOME/sonarqube/bin/{jsw-license,linux-x86-32,macosx*,windows*}                                         && \
+curl  -JL https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-7.2.1.zip -o $HOME/sonarqube.zip    && \
+unzip -q  $HOME/sonarqube.zip -d $HOME                                                                            && \
+rm    -rf $HOME/sonarqube.zip                                                                                     && \
+mv        $HOME/sonarqube-* $HOME/sonarqube                                                                       && \
+rm    -rf $HOME/sonarqube/COPYING                                                                                   && \
+find      $HOME/sonarqube -name "*.txt"   -type f -delete                                                         && \
+rm    -rf $HOME/sonarqube/bin/{jsw-license,linux-x86-32,macosx*,windows*}                                         && \
 sed -i 's/#sonar.jdbc.username=/sonar.jdbc.username=sonarqube/'            $HOME/sonarqube/conf/sonar.properties && \
 sed -i 's/#sonar.jdbc.password=/sonar.jdbc.password=sonarqube123/'         $HOME/sonarqube/conf/sonar.properties && \
 sed -i 's/#sonar.jdbc.url=jdbc:postgresql/sonar.jdbc.url=jdbc:postgresql/' $HOME/sonarqube/conf/sonar.properties && \
