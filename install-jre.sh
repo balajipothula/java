3a54704f6804f3397b54df193030aaf262d98007
BALAJI POTHULA <balan.pothula@gmail.com>
2019-09-04 16:34:25 +0530

    Update install-jre.sh

--- install-jre.sh
+++ install-jre.sh
@@ -8,8 +8,8 @@ curl -JL https://github.com/frekele/oracle-java/releases/download/8u212-b10/jre-
 tar  -xzf $HOME/jre.tar.gz -C $HOME                                                                                        && \
 rm   -rf  $HOME/jre.tar.gz                                                                                                 && \
 mv        $HOME/jre* $HOME/java                                                                                            && \
-find      $HOME/java -maxdepth 2 -name "*.txt"  -type f -delete                                                            && \
-find      $HOME/java -maxdepth 2 -name "*.html" -type f -delete                                                            && \
+find      $HOME/java -name "*.txt"  -type f -delete                                                                        && \
+find      $HOME/java -name "*.html" -type f -delete                                                                        && \
 rm   -rf  $HOME/java/{COPYRIGHT,LICENSE,README,release}                                                                    && \
 rm   -rf  $HOME/java/man                                                                                                   && \
 echo "export PATH=$PATH:$HOME/java/bin" >> $HOME/.bashrc                                                                   && \
