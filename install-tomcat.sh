6429817637af1dcffc741af0040670e8dbe8c1dc
BALAJI POTHULA <balan.pothula@gmail.com>
2019-09-04 16:55:08 +0530

    Update install-tomcat.sh

--- install-tomcat.sh
+++ install-tomcat.sh
@@ -8,7 +8,8 @@ curl -JL http://mirrors.estointernet.in/apache/tomcat/tomcat-8/v8.5.45/bin/apach
 tar  -xzf $HOME/tomcat.tar.gz -C $HOME                                                                                        && \
 rm   -rf  $HOME/tomcat.tar.gz                                                                                                 && \
 mv        $HOME/apache-tomcat-* $HOME/tomcat                                                                                  && \
-find      $HOME/tomcat -name "*.txt" -type f -delete                                                                          && \
 find      $HOME/tomcat -name "*.md"  -type f -delete                                                                          && \
+find      $HOME/tomcat -name "*.txt" -type f -delete                                                                          && \
+find      $HOME/tomcat -name "*.bat" -type f -delete                                                                          && \
 rm   -rf  $HOME/tomcat/{LICENSE,NOTICE,RELEASE-NOTES}                                                                         && \
 rm   -rf  $HOME/tomcat/webapps/{docs,examples,host-manager}
