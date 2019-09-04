2049f68baf7103895e6b4156b0c5566fd70dad4f
BALAJI POTHULA <balan.pothula@gmail.com>
2019-09-04 18:51:28 +0530

    Update install-sonarqube-server.sh

--- install-sonarqube-server.sh
+++ install-sonarqube-server.sh
@@ -9,12 +9,14 @@ sudo yum -y install unzip
 # https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-6.7.7.zip
 # https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-7.2.1.zip
 
+# sed -i 's/#sonar.embeddedDatabase.port=/sonar.embeddedDatabase.port=/'   $HOME/sonarqube/conf/sonar.properties && \
+
 # sonarqube downloading, extracting and running.
 curl -J -L https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-7.2.1.zip -o $HOME/sonarqube.zip    && \
 unzip -q $HOME/sonarqube.zip -d $HOME                                                                            && \
 rm -rf   $HOME/sonarqube.zip                                                                                     && \
 mv       $HOME/sonarqube-* $HOME/sonarqube                                                                       && \
-find     $HOME/sonarqube -name "COPYING" -type f -delete                                                         && \
+rm -rf $HOME/sonarqube/COPYING                                                                                   && \
 find     $HOME/sonarqube -name "*.txt"   -type f -delete                                                         && \
 rm -rf   $HOME/sonarqube/bin/{jsw-license,linux-x86-32,macosx*,windows*}                                         && \
 sed -i 's/#sonar.jdbc.username=/sonar.jdbc.username=sonarqube/'            $HOME/sonarqube/conf/sonar.properties && \
