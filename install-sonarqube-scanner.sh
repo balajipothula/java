d85813799bb3857bae2439a3d46449af0f4326cf
BALAJI POTHULA <balan.pothula@gmail.com>
2019-09-04 19:19:11 +0530

    Update install-sonarqube-scanner.sh

--- install-sonarqube-scanner.sh
+++ install-sonarqube-scanner.sh
@@ -2,21 +2,16 @@
 
 # Author      : BALAJI POTHULA <balaji.pothula@techie.com>,
 # Date        : 16 August 2019,
-# Description : Installing sonar scanner on rhel.
+# Description : Installing SonarQube Scanner from static binaries.
 
 # https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.0.0.1744-linux.zip
 
-# exits any line in the bash script fails.
-set -e
-
-# updating rhel packages.
-sudo yum -y update
-
-# installing openjdk8 unzip.
-sudo yum -y install java-1.8.0-openjdk-devel.x86_64 unzip
+# installing unzip.
+sudo yum -y unzip
 
 # downloading and running sonarscanner.
-curl -J -L https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.0.0.1744-linux.zip -o $HOME/sonar-scanner-cli.zip && \
-unzip -q $HOME/sonar-scanner-cli.zip -d $HOME                                                                                                    && \
-rm -rf   $HOME/sonar-scanner/jre/{legal,release}                                                                                                 && \
-export PATH=$PATH:$HOME/sonar-scanner/bin
+curl  -JL https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.0.0.1744-linux.zip -o $HOME/sonar-scanner-cli.zip && \
+unzip -q  $HOME/sonar-scanner-cli.zip -d $HOME                                                                                                  && \
+rm    -rf $HOME/sonar-scanner/jre/{legal,release}                                                                                               && \
+echo "export PATH=$PATH:$HOME/sonar-scanner/bin" >> $HOME/.bashrc                                                                               && \
+exec $BASH
