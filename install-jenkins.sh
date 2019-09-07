e41b07515e012c5fc6f7f5d8b8d6d6aed611f00a
BALAJI POTHULA <balan.pothula@gmail.com>
2019-09-07 16:06:53 +0530

    Update install-jenkins.sh

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -5,12 +5,11 @@
 # Description : Installing jenkins.war on Tomcat from static binaries.
 
 curl -JL http://mirrors.estointernet.in/apache/tomcat/tomcat-8/v8.5.45/bin/apache-tomcat-8.5.45.tar.gz -o $HOME/tomcat.tar.gz && \
-tar  -xzf $HOME/tomcat.tar.gz -C $HOME                                                                                        && \
-rm   -rf  $HOME/tomcat.tar.gz                                                                                                 && \
-mv        $HOME/apache-tomcat-* $HOME/tomcat                                                                                  && \
-find      $HOME/tomcat -name "*.md"  -type f -delete                                                                          && \
-find      $HOME/tomcat -name "*.txt" -type f -delete                                                                          && \
-find      $HOME/tomcat -name "*.bat" -type f -delete                                                                          && \
-rm   -rf  $HOME/tomcat/{LICENSE,NOTICE,RELEASE-NOTES}                                                                         && \
-rm   -rf  $HOME/tomcat/webapps/{docs,examples,host-manager,manager,ROOT}                                                      && \
+tar -xzf $HOME/tomcat.tar.gz -C $HOME                                                                                         && \
+mv       $HOME/apache-tomcat-* $HOME/tomcat                                                                                   && \
+rm  -rf  $HOME/tomcat.tar.gz                                                                                                  && \
+find     $HOME/tomcat     -maxdepth 1 -name "*.md"  -type f -delete                                                           && \
+find     $HOME/tomcat     -maxdepth 1 -name "*.txt" -type f -delete                                                           && \
+find     $HOME/tomcat/bin -maxdepth 1 -name "*.bat" -type f -delete                                                           && \
+rm  -rf  $HOME/tomcat/webapps/{docs,host-manager,manager,ROOT}                                                                && \
 curl -JL https://updates.jenkins-ci.org/latest/jenkins.war -o $HOME/tomcat/webapps/ROOT.war
