44c187f26c91f82c87c6d5daf3b823f45388a249
BALAJI POTHULA <balan.pothula@gmail.com>
2019-09-07 16:21:24 +0530

    Update install-jenkins.sh

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -12,6 +12,6 @@ find     $HOME/tomcat     -maxdepth 1 -name "*.md"  -type f -delete             
 find     $HOME/tomcat     -maxdepth 1 -name "*.txt" -type f -delete                                                           && \
 find     $HOME/tomcat/bin -maxdepth 1 -name "*.bat" -type f -delete                                                           && \
 rm  -rf  $HOME/tomcat/{LICENSE,NOTICE,RELEASE-NOTES}                                                                          && \
-rm  -rf  $HOME/tomcat/webapps/{docs,examples,host-manager,manager,ROOT}                                                       && \                                                               && \
+rm  -rf  $HOME/tomcat/webapps/{docs,examples,host-manager,manager,ROOT}                                                       && \
 curl -JL https://updates.jenkins-ci.org/latest/jenkins.war -o $HOME/tomcat/webapps/ROOT.war                                   && \
 sh       $HOME/tomcat/bin/startup.sh
