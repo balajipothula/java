ab418afb277fdc3cac2a46006beea8fab9b7571d
BALAJI POTHULA <balan.pothula@gmail.com>
2019-09-09 12:31:59 +0530

    Update install-sonarqube-scanner.sh

--- install-sonarqube-scanner.sh
+++ install-sonarqube-scanner.sh
@@ -12,6 +12,8 @@ sudo yum -y unzip
 # downloading and running sonarscanner.
 curl  -JL https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-4.0.0.1744-linux.zip -o $HOME/sonar-scanner-cli.zip && \
 unzip -q  $HOME/sonar-scanner-cli.zip -d $HOME                                                                                                  && \
-rm    -rf $HOME/sonar-scanner/jre/{legal,release}                                                                                               && \
-echo "export PATH=$PATH:$HOME/sonar-scanner/bin" >> $HOME/.bashrc                                                                               && \
+rm    -rf $HOME/sonar-scanner-cli.zip                                                                                                           && \
+mv        $HOME/sonar-scanner-* sonarscanner                                                                                                    && \
+rm    -rf $HOME/sonarscanner/jre/{legal,release}                                                                                                && \
+echo "export PATH=$PATH:$HOME/sonarscanner/bin" >> $HOME/.bashrc                                                                                && \
 exec $BASH
